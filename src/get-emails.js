8322aa04f4d643ccd1f535c1c8254539232d426d
Boualam3 <boualam.bourahla@gmail.com>
2022-12-13 11:53:53 +0100

    handle errors

--- src/get-emails.js
+++ src/get-emails.js
@@ -3,7 +3,14 @@ const fs = require("fs/promises")
 const getEmails = async () => {
   try {
     const data = await fs.readFile(__dirname + "/resourse/emails.txt", "utf8")
-    const emailsData = data.split("\n")
+    const emailsData = data
+      .split("\n")
+      .filter((item) => /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(item))
+    const invalidEmail = data
+      .split("\n")
+      .filter((item) => !/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(item))
+    if (emailsData.length > 100)
+      throw new Error("Please Provide 100 email addresses")
     return emailsData
   } catch (error) {
     console.log(error)
--- src/index.js
+++ src/index.js
@@ -2,35 +2,46 @@ const nodemailer = require("nodemailer")
 const getEmails = require("./get-emails")
 require("dotenv").config()
 
-async function sendToMultipleEmails(service, myEmail, myPass) {
-  let transporter = nodemailer.createTransport({
-    service: service,
-    auth: {
-      user: myEmail,
-      pass: myPass,
-    },
-  })
-  const emailsData = await getEmails()
+async function sendToMultipleEmails(service, myEmail, myPass, subject, text) {
+  try {
+    let transporter = nodemailer.createTransport({
+      service: service,
+      auth: {
+        user: myEmail,
+        pass: myPass,
+      },
+    })
+    const emailsData = await getEmails()
+    if (!emailsData) return
+    const options = {
+      from: myEmail,
+      to: emailsData,
+      subject: subject,
+      text: text,
+    }
 
-  const options = {
-    from: myEmail,
-    to: emailsData,
-    subject: "Sending email with node js",
-    text: "Test sending to multiple users ",
+    let info = await transporter.sendMail(options)
+
+    console.log("Message sent: %s", info)
+    //* Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+    //* Preview only available when sending through an Ethereal account
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info))
+    //* Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+  } catch (error) {
+    console.log(error)
   }
-
-  let info = await transporter.sendMail(options)
-
-  console.log("Message sent: %s", info)
-  //* Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-  //* Preview only available when sending through an Ethereal account
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info))
-  //* Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 }
 
-sendToMultipleEmails("hotmail", process.env.EMAIL, process.env.PASSWORD).catch(
-  (err) => console.log(err)
+let subject = "Sending email with node js"
+let text = "Test sending to multiple users "
+
+sendToMultipleEmails(
+  "hotmail",
+  process.env.EMAIL,
+  process.env.PASSWORD,
+  subject,
+  text
 )
 
 //!=============================================================
